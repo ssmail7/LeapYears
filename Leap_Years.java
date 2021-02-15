f7587d783670aa490d1f1e099e64a897bb7aa8ba
Steven Smail <41452253+ssmail7@users.noreply.github.com>
2021-02-15 15:57:12 -0500

    Update Leap_Years.java

--- Leap_Years.java
+++ Leap_Years.java
@@ -1,24 +1,45 @@
-//********************************************************************//  Leap_Years.java//  Steven Smail
-////  Determines if a year is a leap year in the Gregorian calendar //********************************************************************import java.util.Scanner;public class Leap_Years{
-  public static void main (String[] args)  {
+//*********************************************************************
+//  Leap_Years.java
+//  Steven Smail
+//
+//  Determines if a year is a leap year in the Gregorian calendar 
+//*********************************************************************
+import java.util.Scanner;
+
+public class Leap_Years
+{
+  public static void main (String[] args)
+  {
     int year;
 
-    Scanner scan = new Scanner (System.in);    System.out.print("Enter a year greater than 1581: ");    year = scan.nextInt();
+    Scanner scan = new Scanner (System.in);
+
+    System.out.print("Enter a year greater than 1581: ");
+    year = scan.nextInt();
     System.out.println("The year is " + (year));
 
     //Checking for year >= 1582 & error message
-    if (year < 1582)    {      System.out.println("The year must be greater than 1581");
+    if (year < 1582)
+    {
+      System.out.println("The year must be greater than 1581");
       System.out.println();
     }
-    else    {
+
+    else
+    {
       //Checking for divisible by 4
-      if (year % 4 != 0)      {        System.out.println((year) + " is not a leap year");
+      if (year % 4 != 0)
+      {
+        System.out.println((year) + " is not a leap year");
         System.out.println();
       }
-      else      {
+
+      else
+      {
         //Checking for divisible by 100
         if (year % 100 != 0)
-        {          System.out.println((year) + " is a leap year");
+        {
+          System.out.println((year) + " is a leap year");
           System.out.println();
         }
 
@@ -36,5 +57,7 @@
             System.out.println();
           }
         }
-      }    }   }
+      }
+    }
+   }
 }
